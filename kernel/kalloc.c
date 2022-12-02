b00f93992f88c10c42665a424a78f8e6feb252b6
Yifan Zhao <stopire@gmail.com>
2022-12-03 02:14:14 +0800

    finish kmem allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,14 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) {
+    initlock(&kmem[i].lock, "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -47,6 +49,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int cpu;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +59,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  /* Use cpuid() only when interrupt is off. */
+  push_off();
+  cpu = cpuid();
+  acquire(&kmem[cpu].lock);
+
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+
+  release(&kmem[cpu].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +77,80 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+  struct run *r, *new_head;
+  int cpu;
+  int cur_size;
+  int retry = 0;
+  const int STEAL_SIZE = 1024;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+beg:
+  /* local allocation */
+  push_off();
+  cpu = cpuid();
+  acquire(&kmem[cpu].lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  r = kmem[cpu].freelist;
+  if (r) kmem[cpu].freelist = r->next;
+
+  release(&kmem[cpu].lock);
+  pop_off();
+
+  /* local allocation fails, try to steal other's page */
+  if (!r) {
+    for (int i = 0; i < NCPU; i++) {
+      /* take victim's lock */
+      acquire(&kmem[i].lock);
+      cpu = cpuid();
+
+      /* victim shouldn't be myself */
+      if (i == cpu) {
+        release(&kmem[i].lock);
+        continue;
+      }
+
+      /* victim is also poor, find next victim */
+      r = kmem[i].freelist;
+      if (!r) {
+        release(&kmem[i].lock);
+        continue;
+      }
+
+      /* try to fetch 1024 pages, or as much as possible */
+      new_head = r;
+      cur_size = 1;
+      while (r->next && cur_size != STEAL_SIZE) {
+        r = r->next;
+        ++cur_size;
+      }
+
+      /* cut stolen pages off from victim's list */
+      kmem[i].freelist = r->next;
+      r->next = (void *)0;
+
+      /* Take my lock to insert stolen pages.
+       * DO NOT worry about deadlock.
+       * I have no free page so nobody may steal from me, thus not take my lock.
+       */
+      acquire(&kmem[cpu].lock);
+      r = new_head;
+      kmem[cpu].freelist = r->next;
+      release(&kmem[cpu].lock);
+
+      release(&kmem[i].lock);
+
+      break;
+    }
+  }
+
+  if (r) {
+    memset((char *)r, 5, PGSIZE); // fill with junk
+    return (void *)r;
+  } else if (!retry) {
+    /* retry one more time when failure */
+    retry = 1;
+    goto beg;
+  }
+
+  /* really failure */
+  return (void *)0;
 }
